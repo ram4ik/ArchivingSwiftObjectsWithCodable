f6449ed5ed0a01870586f772a2cfe69ae2cee7b4
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-31 10:32:32 +0200

    save userData

--- ArchivingSwiftObjectsWithCodable/ContentView.swift
+++ ArchivingSwiftObjectsWithCodable/ContentView.swift
@@ -14,8 +14,15 @@ struct User: Codable {
 }
 
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
     var body: some View {
-        Text("Hello World")
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
